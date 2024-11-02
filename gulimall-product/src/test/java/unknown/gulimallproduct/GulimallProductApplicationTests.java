d0ab815e33f8141660b95a9003118de35b0f3713
123yujin <116158652+123yujin@users.noreply.github.com>
2024-11-02 17:02:26 +0800

    feat(auth):项目初始化结构

---
+++ gulimall-product/src/test/java/unknown/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package unknown.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
